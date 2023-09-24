dafff9d4afab1f2c84dba3ad7a47884e3002f38c
Lakshmi Prasanna Malempati <lm1298@usnh.edu>
2023-09-24 13:30:21 -0400

    Use CheeseFactory in a test

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -2,11 +2,9 @@ import pytest
 # Connects our tests with our database
 pytestmark = pytest.mark.django_db
 from ..models import Cheese
+from .factories import CheeseFactory
+
 def test__str__():
-    cheese = Cheese.objects.create(
-        name="Stracchino",
-        description="Semi-sweet cheese eaten with starches.",
-        firmness=Cheese.Firmness.SOFT,
-    )
+    cheese = CheeseFactory(name="Stracchino")
     assert cheese.__str__() == "Stracchino"
     assert str(cheese) == "Stracchino"
--- everycheese/users/tests/test_models.py
+++ everycheese/users/tests/test_models.py
@@ -1,16 +1,8 @@
 import pytest
 
 from everycheese.users.models import User
-from ..models import Cheese
+
 pytestmark = pytest.mark.django_db
 
-def test__str__():
-    cheese = Cheese.objects.create(
-        name="Stracchino",
-        description="Semi-sweet cheese eaten with starches.",
-        firmness=Cheese.Firmness.SOFT,
-    )
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
 def test_user_get_absolute_url(user: User):
     assert user.get_absolute_url() == f"/users/{user.username}/"
